9381dc91ce90296f417cf52ab0e469a09901d090
878652265 <qwer9112077@gmail.com>
2025-03-22 03:45:53 +0800

    First trial

--- student_agent.py
+++ student_agent.py
@@ -3,6 +3,8 @@ import numpy as np
 import pickle
 import random
 import gym
+from simple_custom_taxi_env import SimpleTaxiEnv
+
 
 def get_action(obs):
     
@@ -12,7 +14,14 @@ def get_action(obs):
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
 
-
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
+    # read q_table
+    with open('q_table.pkl', 'rb') as f:
+        q_table = pickle.load(f)
+    # get state
+    taxi_row, taxi_col, _,_,_,_,_,_,_,_,obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look = obs
+    state = ( obstacle_south, obstacle_north, obstacle_east, obstacle_west)
+    if state not in q_table:
+        return random.choice([0, 1, 2, 3, 4, 5])
+    action = np.argmax(q_table[state])
+    return action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
-
