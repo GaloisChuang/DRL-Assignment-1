8d1bcb04d35d752d5505ae541a7fe63fb8b7f408
878652265 <qwer9112077@gmail.com>
2025-03-24 04:12:56 +0800

    4900

--- student_agent.py
+++ student_agent.py
@@ -99,7 +99,7 @@ def get_action(obs):
                     relative_goal_pos = (goal[0] - taxi_row, goal[1] - taxi_col)
                     state = ( obstacle_south, obstacle_north, obstacle_east, obstacle_west, relative_goal_pos[0], relative_goal_pos[1], globals.has_passenger)
     
-    if globals.fuel < 1:
+    if globals.fuel < 4900:
         q_table = globals.lazy_q_table
         if state not in q_table:
             action = random.randint(0, 5)
